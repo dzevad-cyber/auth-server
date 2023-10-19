db1b7991bdd7bc876ea969e217868e58afe88d27
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-20 00:08:40 +0200

    feat: install swc for 10x speed in boot time

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,10 @@
+console.time('Server boot time');
 import express from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  console.timeEnd('Server boot time');
+});
