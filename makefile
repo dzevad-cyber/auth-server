9aaeadd87c6e4f5da4c33a1a3a27699a2608750c
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-20 17:10:09 +0200

    feat: create server db connection
    fix: add db connection
    
    feat: add connection to db, setup health check for containers

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 # enviroment development
 du: 
-	docker-compose -f docker-compose.dev.yml up 
+	docker-compose -f docker-compose.dev.yml up -d && docker logs -f auth-server-container
 dd: 
-	docker-compose -f docker-compose.dev.yml down -v
+	docker-compose -f docker-compose.dev.yml down
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,30 @@
+import { Sequelize } from 'sequelize';
 console.time('Server startup time');
 import express from 'express';
 
 const app = express();
 
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE!,
+  process.env.MYSQL_USER!,
+  process.env.MYSQL_PASSWORD!,
+  {
+    host: 'mysql_auth',
+    dialect: 'mysql',
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!');
 });
 
 const PORT = process.env.PORT || 5000;
